f51f25a5411f7cf6e81d15ccb05d9a649f3b7ba8
hhidatasettechs <hhidatasettechs@gmail.com>
2026-03-22 06:06:27 -0500

    Stabilize checksum script and add baseline

--- audits/checksums/generate_checksums.sh
+++ audits/checksums/generate_checksums.sh
@@ -1,2 +1,2 @@
 #!/data/data/com.termux/files/usr/bin/bash
-find audits -type f -exec sha256sum {} \\; > audits/checksums/sha256_manifest.txt
+find audits -type f ! -name "sha256_manifest.txt" -exec sha256sum {} \; > audits/checksums/sha256_manifest.txt
